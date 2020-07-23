05d079c7742e024195cfd55a30d254c93179d0fb
hakyulee <dlgkrbek123@cashwalk.io>
2020-07-23 13:08:29 +0900

    reducer, saga 정리

--- front/sagas/post.js
+++ front/sagas/post.js
@@ -1,5 +1,13 @@
 import { all, takeLatest, put, fork, delay } from 'redux-saga/effects';
 import axios from 'axios';
+import {
+  ADD_POST_REQUEST,
+  ADD_POST_SUCCESS,
+  ADD_POST_FAILURE,
+  ADD_COMMENT_REQUEST,
+  ADD_COMMENT_SUCCESS,
+  ADD_COMMENT_FAILURE,
+} from '../reducers/post';
 
 function addPostAPI() {
   return axios.post('/api/post');
@@ -11,21 +19,44 @@ function* addPost() {
     yield delay(1000);
 
     yield put({
-      type: 'ADD_POST_SUCCESS',
-      data: result.data,
+      type: ADD_POST_SUCCESS,
+      // data: result.data,
     });
   } catch (err) {
     yield put({
-      type: 'ADD_POST_FAILURE',
-      data: err.response.data,
+      type: ADD_POST_FAILURE,
+      error: err.response.data,
+    });
+  }
+}
+
+function addCommentAPI(data) {
+  return axios.post(`/api/post/${data.postId}/comment`, data);
+}
+
+function* addComment() {
+  try {
+    yield delay(1000);
+    yield put({
+      type: ADD_COMMENT_SUCCESS,
+      // data: result.data,
+    });
+  } catch (err) {
+    yield put({
+      type: ADD_COMMENT_FAILURE,
+      error: err.response.data,
     });
   }
 }
 
 function* watchAddPost() {
-  yield takeLatest('ADD_POST_REQUEST', addPost);
+  yield takeLatest(ADD_POST_REQUEST, addPost);
+}
+
+function* watchAddComment() {
+  yield takeLatest(ADD_COMMENT_REQUEST, addComment);
 }
 
 export default function* postSaga() {
-  yield all([fork(watchAddPost)]);
+  yield all([fork(watchAddPost), fork(watchAddComment)]);
 }
